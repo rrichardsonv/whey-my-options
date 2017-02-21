1fe8a6f0abc3b2f1012ab40bdfda2e583aa313f0
rrichardsonv <rrichardsonv+github@gmail.com>
2017-02-21 11:47:25 -0600

    register route responds to request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
-    #partial for reg
   end
 
   def create
+    @user = User.new(user_deets)
+    if @user.save
+      redirect_to restaurants_path
+    else
+      flash[:alert] = "Incorrectly formatted"
+    end
+  end
+
+  def user_deets
+    params.require(:user).permit(:email, :password, :username)
   end
 end
