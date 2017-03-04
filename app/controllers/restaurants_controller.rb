f78a9fcbf2e0df978df5c99f0178be68d03eff78
rrichardsonv <rrichardsonv+github@gmail.com>
2017-03-04 15:40:23 -0600

    Got server to respone with partials restaurant panes are working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,25 @@
 class RestaurantsController < ApplicationController
+  include RestaurantsHelper
   def index
-    @restaurants = Restaurant.all
-    render :index, layout: false
+    if request.xhr?
+      user_location = {
+        coords: {
+          latitude: location_params[:lat],
+          longitude: location_params[:long]  
+        }
+        
+      }
+      near_restaurants = get_restaurant_panes(user_location)
+      # FOR TESTING SO YOU DON'T USE ALL THE API CALLS
+      # near_restaurants = Restaurant.all
+
+      render partial: 'index', layout: false, locals: {restaurants: near_restaurants} , status: :ok
+    else
+      render :index
+    end
+  end
+private
+  def location_params
+    params.require(:coords).permit(:lat, :long)
   end
 end
