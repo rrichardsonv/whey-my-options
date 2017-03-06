4595603ede31f5e87521e0e18402c2c130cf9fe7
Ashley Yang <ashley.minghui@gmail.com>
2017-03-06 12:19:05 -0600

    Temporarily change #current_user in SessionsHelper to return first user for creating favorites while Rich is working on user authentication. Still need to fix route for creating a new favorite from the restaurant show page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,7 @@
 module SessionsHelper
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    # @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find(1)
   end
 
   def logged_in?
