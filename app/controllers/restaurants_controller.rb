869ebf8b55623fe4d869233524dcc776b2e75db2
rrichardsonv <rrichardsonv+github@gmail.com>
2017-03-06 14:28:19 -0600

    reverted some commenting for testing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,23 +1,22 @@
 class RestaurantsController < ApplicationController
   include RestaurantsHelper
   def index
-    @restaurants = Restaurant.all
-    # if !!location_params
-    #   user_location = {
-    #     coords: {
-    #       latitude: location_params[:lat],
-    #       longitude: location_params[:long]
-    #     }
+    if !!location_params
+      user_location = {
+        coords: {
+          latitude: location_params[:lat],
+          longitude: location_params[:long]
+        }
 
-    #   }
-    #   # near_restaurants = get_restaurant_panes(user_location)
-    #   # FOR TESTING SO YOU DON'T USE ALL THE API CALLS
-    #   near_restaurants = Restaurant.all
+      }
+      # near_restaurants = get_restaurant_panes(user_location)
+      # FOR TESTING SO YOU DON'T USE ALL THE API CALLS
+      near_restaurants = Restaurant.all
 
-    #   render partial: 'index', layout: false, locals: {restaurants: near_restaurants} , status: :ok
-    # else
-    #   @restaurants = Restaurant.all
-    # end
+      render partial: 'index', layout: false, locals: {restaurants: near_restaurants} , status: :ok
+    else
+      @restaurants = Restaurant.all
+    end
   end
 
 private
